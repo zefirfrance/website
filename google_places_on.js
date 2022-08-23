9b90b12d4043816b168ef63d5f3643b4b5cd6eaf
Julian Bilcke <julian.bilcke@zefir.fr>
2022-08-23 17:25:24 +0200

    ✨ feat(front) BS-675: Redirect users to the new webform using path (not the domain)

--- google_places_on.js
+++ google_places_on.js
@@ -1,23 +1,27 @@
-object_2 = document.getElementById("goingnext-2")
-object_5 = document.getElementById("goingnext-5")
+object_2 = document.getElementById("goingnext-2");
+object_5 = document.getElementById("goingnext-5");
 
-object_2.type = "submit"
-object_5.type = "submit"
+object_2.type = "submit";
+object_5.type = "submit";
 
 object_2.addEventListener("click", ErrorMessage);
 object_5.addEventListener("click", ErrorMessage);
 
+function redirectToSellerForm() {
+  window.location.pathname = "/offre/demande/v2";
+}
+
 function ErrorMessage() {
   if (!document.getElementById("autocomplete_2").value.match(/^\d/)) {
-    document.getElementById("card-body-2").style.display = 'block';
-    setTimeout(function(){
-      document.getElementById("card-body-2").style.display = 'none';
+    document.getElementById("card-body-2").style.display = "block";
+    setTimeout(function () {
+      document.getElementById("card-body-2").style.display = "none";
     }, 5000);
   }
   if (!document.getElementById("autocomplete_5").value.match(/^\d/)) {
-    document.getElementById("card-body-2").style.display = 'block';
-    setTimeout(function(){
-      document.getElementById("card-body-2").style.display = 'none';
+    document.getElementById("card-body-2").style.display = "block";
+    setTimeout(function () {
+      document.getElementById("card-body-2").style.display = "none";
     }, 5000);
   }
 }
@@ -29,7 +33,7 @@ var options = {
   useEasing: true,
   useGrouping: true,
   separator: "",
-  decimal: "."
+  decimal: ".",
 };
 
 // This sample uses the Autocomplete widget to help the user select a
@@ -39,58 +43,58 @@ var options = {
 var placeSearch, autocomplete;
 
 var componentForm = {
-  street_number: 'short_name',
-  route: 'long_name',
-  locality: 'long_name',
-  administrative_area_level_1: 'short_name',
-  country: 'long_name',
-  postal_code: 'short_name'
+  street_number: "short_name",
+  route: "long_name",
+  locality: "long_name",
+  administrative_area_level_1: "short_name",
+  country: "long_name",
+  postal_code: "short_name",
 };
 
 function initAutocomplete_2() {
   // Create the autocomplete object, restricting the search predictions to
-  // adresses and France.
+  // addresses and France.
   // Address must start with a number to start Autocomplete
 
-  var input = document.getElementById('autocomplete_2');
+  var input = document.getElementById("autocomplete_2");
 
   var options = {
-    types: ['address'],
-    componentRestrictions: {country: 'fr'},
+    types: ["address"],
+    componentRestrictions: { country: "fr" },
   };
 
   autocomplete = new google.maps.places.Autocomplete(input, options);
 
   // Avoid paying for data that you don't need by restricting the set of
   // place fields that are returned to just the address components.
-  autocomplete.setFields(['address_component']);
+  autocomplete.setFields(["address_component"]);
 
   // When the user selects an address from the drop-down, save the
   // address fields in local storage.
-  autocomplete.addListener('place_changed', fillInAddress_2);
+  autocomplete.addListener("place_changed", fillInAddress_2);
 }
 
 function initAutocomplete_5() {
   // Create the autocomplete object, restricting the search predictions to
-  // adresses and France.
+  // addresses and France.
   // Address must start with a number to start Autocomplete
 
-  var input = document.getElementById('autocomplete_5');
+  var input = document.getElementById("autocomplete_5");
 
   var options = {
-    types: ['address'],
-    componentRestrictions: {country: 'fr'},
+    types: ["address"],
+    componentRestrictions: { country: "fr" },
   };
 
   autocomplete = new google.maps.places.Autocomplete(input, options);
 
   // Avoid paying for data that you don't need by restricting the set of
   // place fields that are returned to just the address components.
-  autocomplete.setFields(['address_component']);
+  autocomplete.setFields(["address_component"]);
 
   // When the user selects an address from the drop-down, save the
   // address fields in local storage.
-  autocomplete.addListener('place_changed', fillInAddress_5);
+  autocomplete.addListener("place_changed", fillInAddress_5);
 }
 
 function patternMatching_2() {
@@ -124,14 +128,16 @@ function fillInAddress_2() {
     for (var i = 0; i < place_2.address_components.length; i++) {
       var addressType = place_2.address_components[i].types[0];
       if (componentForm[addressType]) {
-        var val = encodeURIComponent(place_2.address_components[i][componentForm[addressType]]);
+        var val = encodeURIComponent(
+          place_2.address_components[i][componentForm[addressType]]
+        );
         // Store the home address in a cookie
         var homecookie = addressType + "=" + val;
-        var path = "path=/"
-        document.cookie = homecookie + ';' + path;
+        var path = "path=/";
+        document.cookie = homecookie + ";" + path;
       }
     }
-    window.location.assign("https://zefir.fr/offre/demande/v2");
+    redirectToSellerForm();
   } else {
     ErrorMessage();
   }
@@ -146,25 +152,29 @@ function fillInAddress_5() {
     for (var i = 0; i < place_5.address_components.length; i++) {
       var addressType = place_5.address_components[i].types[0];
       if (componentForm[addressType]) {
-        var val = encodeURIComponent(place_5.address_components[i][componentForm[addressType]]);
+        var val = encodeURIComponent(
+          place_5.address_components[i][componentForm[addressType]]
+        );
         // Store the home address in a cookie
         var homecookie = addressType + "=" + val;
-        var path = "path=/"
-        document.cookie = homecookie + ';' + path;
+        var path = "path=/";
+        document.cookie = homecookie + ";" + path;
       }
     }
-    window.location.assign("https://zefir.fr/offre/demande/v2");
+    redirectToSellerForm();
   } else {
     ErrorMessage();
   }
 }
 
-var pac_input_2 = document.getElementById('autocomplete_2');
-var pac_input_5 = document.getElementById('autocomplete_5');
+var pac_input_2 = document.getElementById("autocomplete_2");
+var pac_input_5 = document.getElementById("autocomplete_5");
 
-(function pacSelectFirst(input){
+(function pacSelectFirst(input) {
   // store the original event binding function
-  var _addEventListener = (input.addEventListener) ? input.addEventListener : input.attachEvent;
+  var _addEventListener = input.addEventListener
+    ? input.addEventListener
+    : input.attachEvent;
 
   function addEventListenerWrapper(type, listener) {
     // Simulate a 'down arrow' keypress on hitting 'return' when no pac suggestion is selected,
@@ -174,16 +184,29 @@ var pac_input_5 = document.getElementById('autocomplete_5');
       var orig_listener = listener;
       listener = function (event) {
         var suggestion_selected = $(".pac-item-selected").length > 0;
-        document.getElementById('goingnext-2').onclick = function() {
-          if (!suggestion_selected && document.getElementById("autocomplete_2").value.match(/^\d/)) {
-            var simulated_downarrow = $.Event("keydown", {keyCode:40, which:40});
-            var event = $.Event("enter", {keyCode:13, which:13})
+        document.getElementById("goingnext-2").onclick = function () {
+          if (
+            !suggestion_selected &&
+            document.getElementById("autocomplete_2").value.match(/^\d/)
+          ) {
+            var simulated_downarrow = $.Event("keydown", {
+              keyCode: 40,
+              which: 40,
+            });
+            var event = $.Event("enter", { keyCode: 13, which: 13 });
             orig_listener.apply(input, [simulated_downarrow]);
             orig_listener.apply(input, [event]);
           }
-        }
-        if (event.which == 13 && !suggestion_selected && document.getElementById("autocomplete_2").value.match(/^\d/)) {
-          var simulated_downarrow = $.Event("keydown", {keyCode:40, which:40})
+        };
+        if (
+          event.which == 13 &&
+          !suggestion_selected &&
+          document.getElementById("autocomplete_2").value.match(/^\d/)
+        ) {
+          var simulated_downarrow = $.Event("keydown", {
+            keyCode: 40,
+            which: 40,
+          });
           orig_listener.apply(input, [simulated_downarrow]);
         }
         orig_listener.apply(input, [event]);
@@ -193,28 +216,32 @@ var pac_input_5 = document.getElementById('autocomplete_5');
     _addEventListener.apply(input, [type, listener]);
   }
 
-  if (input.addEventListener)
-    input.addEventListener = addEventListenerWrapper;
-  else if (input.attachEvent)
-    input.attachEvent = addEventListenerWrapper;
-
+  if (input.addEventListener) input.addEventListener = addEventListenerWrapper;
+  else if (input.attachEvent) input.attachEvent = addEventListenerWrapper;
 })(pac_input_2);
 
-$(function(){
-  var autocompleteOptions = { types: ['address'], componentRestrictions: { country: 'fr'}};
-  var autocomplete = new google.maps.places.Autocomplete(pac_input_2, autocompleteOptions);
+$(function () {
+  var autocompleteOptions = {
+    types: ["address"],
+    componentRestrictions: { country: "fr" },
+  };
+  var autocomplete = new google.maps.places.Autocomplete(
+    pac_input_2,
+    autocompleteOptions
+  );
 });
 
-$(document).ready(function(){
-  $(window).scroll(function(){
-  	var isFocused =  $("#autocomplete_2").is(":focus") 
-  	if (isFocused == true){
-    //Set new top to autocomplete dropdown
-    newTop = $('#autocomplete_2').offset().top + $('#autocomplete_2').outerHeight();
-    $('.pac-container').css('top', newTop + 'px');
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var isFocused = $("#autocomplete_2").is(":focus");
+    if (isFocused == true) {
+      //Set new top to autocomplete dropdown
+      newTop =
+        $("#autocomplete_2").offset().top + $("#autocomplete_2").outerHeight();
+      $(".pac-container").css("top", newTop + "px");
     }
-  })
-  $('#autocomplete_2').blur(function(){
-    $('.pac-container').css('top', '0px');
-  })
+  });
+  $("#autocomplete_2").blur(function () {
+    $(".pac-container").css("top", "0px");
+  });
 });
